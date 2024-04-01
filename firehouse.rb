8008b56e7449443b25bf6a8e3a63e7448428fae5
izqalan <izqalan@gmail.com>
2024-04-02 03:32:45 +0800

    Brew formula update for firehouse version v0.5.10

--- firehouse.rb
+++ firehouse.rb
@@ -5,11 +5,11 @@
 class Firehouse < Formula
   desc ""
   homepage "https://github.com/izqalan/firehouse"
-  version "0.5.9"
+  version "0.5.10"
 
   on_macos do
-    url "https://github.com/izqalan/firehouse/releases/download/v0.5.9/firehouse_0.5.9_darwin_all.tar.gz"
-    sha256 "3c9206d46df9a870775fc39aae5d5c4cb935ece806994f1aac120021dfb398f1"
+    url "https://github.com/izqalan/firehouse/releases/download/v0.5.10/firehouse_0.5.10_darwin_all.tar.gz"
+    sha256 "c94c269161f880459162730cc67e6655810ed5c511a817240c524a897d63bd4b"
 
     def install
       bin.install "firehouse"
@@ -17,17 +17,17 @@ class Firehouse < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/izqalan/firehouse/releases/download/v0.5.9/firehouse_0.5.9_linux_amd64.tar.gz"
-      sha256 "a28537211c6fe5b74c37d23181773cb461d7a8a1fe32a1e53387747fae2218fd"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/izqalan/firehouse/releases/download/v0.5.10/firehouse_0.5.10_linux_arm64.tar.gz"
+      sha256 "584b250e4985d68db2a303b2f3cf145924645f1488a2fb64c83d12ea84ba9f18"
 
       def install
         bin.install "firehouse"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/izqalan/firehouse/releases/download/v0.5.9/firehouse_0.5.9_linux_arm64.tar.gz"
-      sha256 "ea966fc97b6b441b4a7dab7ea2ecf646e0ea49697130ecb90a5d809c107e6feb"
+    if Hardware::CPU.intel?
+      url "https://github.com/izqalan/firehouse/releases/download/v0.5.10/firehouse_0.5.10_linux_amd64.tar.gz"
+      sha256 "2d600e9438cb90f0e2e15975d34caa4effd1ee342cc985b3d3b34ecd35e6296e"
 
       def install
         bin.install "firehouse"
